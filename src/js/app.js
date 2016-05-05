4ce3b604a9ba70a74cfc26af088795857a0e5f12
Bruno Germano <bruno.germano@gmail.com>
2016-05-05 19:42:33 +0000

    implements ajax and multiples indexes

--- src/js/app.js
+++ src/js/app.js
@@ -4,85 +4,114 @@
  * This is where you write your app.
  */
 
-var UI = require('ui');
-var Vector2 = require('vector2');
+var UI = require('ui'),
+    ajax = require('ajax');
+
+var sources = [
+    {
+      uid: 'dolar',
+      title: 'Dolar',
+      url: 'http://developers.agenciaideias.com.br/cotacoes/json',
+      responseData: {},
+      parse: function(data) {
+        return data.dolar.cotacao + '(' + data.dolar.variacao + ')';
+      }
+    },
+    {
+      uid: 'bovespa',
+      title: 'Bovespa',
+      url: 'http://developers.agenciaideias.com.br/cotacoes/json',
+      responseData: {},
+      parse: function(data) {
+        return data.bovespa.variacao;
+      }
+    },
+    {
+      uid: 'mercado',
+      title: 'Mercado Biticoin',
+      url: 'https://www.mercadobitcoin.net/api/ticker/',
+      responseData: {},
+      parse: function(data) {
+        return data.ticker.last;
+      }
+    },
+    {
+      uid: 'btc2u',
+      title: 'Bitcoins 2 You',
+      url: 'https://www.bitcointoyou.com/api/ticker.aspx',
+      responseData: {},
+      parse: function(data) {
+        return data.ticker.last;
+      }
+    },
+    {
+      uid: 'Fox Bit',
+      title: 'Fox Bit',
+      url: 'https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC',
+      responseData: {},
+      parse: function(data) {
+        return data.last;
+      }
+    },
+  ],
+  dataSources = [],
+  currentIndex = 0;
+
+function loadData () {
+  var done = 0;
+
+  for (var i = 0; i < sources.length; i++) {
+    // Make the request
+
+    ajax(
+      {
+        url: sources[i].url,
+        type: 'json'
+      },
+      function(res) {
+        // Success!
+        done++;
+        dataSources.push(res);
+
+        if (done==sources.length) {
+          currentIndex=0;
+          showIndex(currentIndex);
+        }
+      }
+    );
+  }
+
+}
+
+function showIndex (index) {
+  main.title(sources[currentIndex].title);
+  main.subtitle(sources[currentIndex].parse(dataSources[currentIndex]));
+}
 
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
+  title: 'Carregando',
+  subtitle: 'Aguarde...'
 });
 
 main.show();
+loadData();
 
 main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+  currentIndex = --currentIndex;
+  currentIndex = currentIndex < 0 ? currentIndex=sources.length - 1 : currentIndex;
+
+  showIndex(currentIndex);
 });
 
 main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
+  main.title('Atualizando');
+  main.subtitle('Aguarde...');
+  loadData();
 });
 
 main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+  currentIndex = ++currentIndex;
+  currentIndex = currentIndex >= sources.length ? 0 : currentIndex;
+
+  showIndex(currentIndex);
 });
